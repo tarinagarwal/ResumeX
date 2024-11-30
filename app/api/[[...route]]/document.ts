9b980f106651cafdd85ddf38ed0efed6a2b28f85
tarinagarwal <theultimatemania64@gmail.com>
2024-12-01 02:26:08 +0530

    add landing page and setup vercel db

---
+++ app/api/[[...route]]/document.ts
@@ -0,0 +1,5 @@
+import { Hono } from "hono";
+
+const documentRoute = new Hono()
+
+export default documentRoute;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import dotenv from "dotenv";
+dotenv.config();
+
+const nextConfig = {
+  images: {
+    domains: ["picsum.photos"],
+  },
+};
 
 export default nextConfig;
